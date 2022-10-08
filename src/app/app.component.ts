cdeb6ff636af6425b45b496da3f3b0d3af9c90e4
Sebazt <juan.londono29@udea.edu.co>
2022-10-07 19:17:12 -0500

    finish workHome 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title:String = 'Pokemon';
-  favorite:String = 'Charizard';
+  title: String = 'Pokemon';
+  favorite: String = 'Charizard';
+  img: String =
+    'https://thecatapi.com/api/images/get?format=src&type=gif?results_per_page=';
 }
